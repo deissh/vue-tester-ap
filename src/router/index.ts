a8666438e5fff9ee8e8d7247d18d7d10b8a1949f
deissh <deissh@yandex.ru>
2020-12-15 10:49:50 +0300

    init project

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,12 +8,13 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/auth',
+    redirect: '/auth/sign_in', // default path
+    component: import('../views/auth/BaseView.vue'),
+    children: [{
+      path: 'sign_in',
+      component: import('../views/auth/SignIn.vue'),
+    }],
   },
 ];
 
--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,13 +1,15 @@
 <template>
-$END$
+  <div class="home">
+    asd
+  </div>
 </template>
 
-<script>
-export default {
-name: "SignIn.vue"
-}
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class SignIn extends Vue {}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
